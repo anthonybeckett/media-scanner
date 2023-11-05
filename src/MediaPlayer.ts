63b96db112c25ebaf4eb057d8f1b354b698697c6
Anthony Beckett <antneyb10@gmail.com>
2023-11-05 11:33:24 +0000

    Added testing code to request information from the WinRT binary to fetch information from windows media player

--- src/MediaPlayer.ts
+++ src/MediaPlayer.ts
@@ -1,6 +1,15 @@
 import MediaApplicationInterface from "./Interfaces/MediaApplicationInterface.js";
 import Serial from "./Serial.js";
 import { autoInjectable } from 'tsyringe';
+import { exec } from "child_process";
+import { __dirname } from "./Helpers/Directory.js";
+
+interface ExecException extends Error {
+    cmd?: string | undefined;
+    killed?: boolean | undefined;
+    code?: number | undefined;
+    signal?: NodeJS.Signals | undefined;
+}
 
 @autoInjectable()
 export default class MediaPlayer implements MediaApplicationInterface
@@ -15,6 +24,17 @@ export default class MediaPlayer implements MediaApplicationInterface
     run(): void
     {
         this._serial.sendMessage("Running media player");
+        setInterval(() => {
+            exec(__dirname + "\\..\\..\\..\\Bin\\WinRtTest2.exe", (error: ExecException | null, stdout: string, stderr: string): void => {
+                // To-do: Parse JSON before sending to device
+
+                console.log(stdout);
+                console.log(error);
+                console.log(stderr);
+                //this._serial.sendMessage(stdout);
+            });
+        }, 1000);
+        
         // Implement code to update every 2 seconds
     }
 }
